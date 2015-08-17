7621dc4febe2d591613a21471fc7b40900840136
Elektron1c97 <Elektron1c97@gmail.com>
2015-08-17 13:27:36 +0200

    added tcpserver, added function to restart a game

--- lib/mastermind_ruby.rb
+++ lib/mastermind_ruby.rb
@@ -1,3 +1,4 @@
 require_relative 'mastermind_ruby/console_interface'
+require_relative 'mastermind_ruby/input_output/terminal_io'
 require_relative 'mastermind_ruby/game'
 require_relative 'mastermind_ruby/code'
